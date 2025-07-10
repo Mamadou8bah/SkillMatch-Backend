56c8283f2e84b86b8631a0c68dd0443c24fa740d
mamadou8bah <mbah18791@gmail.com>
2025-07-10 22:20:59 +0000

    Implemented the JwtUtil class which includes the logic to generate token

---
+++ src/main/java/SkillMatch/util/JwtUtil.java
@@ -0,0 +1,49 @@
+package SkillMatch.util;
+
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final long EXPIRATION_TIME = 86400000; // 1 day in ms
+
+    public JwtUtil(@Value("$jwt.secret") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateKey(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256,key)
+                .compact();
+    }
+
+    public String extractUserNameFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            return extractUserNameFromToken(token) != null;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
