e19b28dd96d3fd9e4e9dafb5a0c5780f17545426
mamadou8bah <mbah18791@gmail.com>
2025-07-11 16:02:48 +0000

    Implemented a DTO for login response

--- src/main/java/SkillMatch/dto/LoginRequest.java
+++ src/main/java/SkillMatch/dto/LoginRequest.java
@@ -1,10 +1,16 @@
 package SkillMatch.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
 
 
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 
     public String getEmail() {
