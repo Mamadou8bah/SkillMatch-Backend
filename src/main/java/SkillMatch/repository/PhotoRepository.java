ffe5a471ee771c44cf61b271521c4f10ba6ff523
mamadou8bah <mbah18791@gmail.com>
2025-08-16 09:26:23 +0000

    Implemented Photo service

--- src/main/java/SkillMatch/repository/PhotoRepository.java
+++ src/main/java/SkillMatch/repository/PhotoRepository.java
@@ -1,5 +1,7 @@
 package SkillMatch.repository;
 
+import SkillMatch.model.Candidate;
+import SkillMatch.model.Employer;
 import SkillMatch.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,6 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
     Photo findByUrl(String url);
     Photo findByPublicId(String publicId);
+    Photo findByCandidate(Candidate candidate);
+    Photo findByEmployer(Employer employer);
 }
---
+++ src/main/java/SkillMatch/service/PhotoService.java
@@ -0,0 +1,68 @@
+package SkillMatch.service;
+
+import SkillMatch.model.Candidate;
+import SkillMatch.model.Employer;
+import SkillMatch.model.Photo;
+import SkillMatch.repository.PhotoRepository;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Service
+public class PhotoService {
+
+    @Autowired
+    private CloudinaryService cloudinaryService;
+
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    public Photo createPhoto(MultipartFile photoFile)throws IOException {
+
+        try{
+            Map photo =cloudinaryService.uploadImage(photoFile);
+            String url = photo.get("url").toString();
+            String publicId = photo.get("public_id").toString();
+
+            Photo newPhoto = new Photo();
+            newPhoto.setUrl(url);
+            newPhoto.setPublicId(publicId);
+            return  photoRepository.save(newPhoto);
+        }catch(Exception e){
+            throw new IOException("Failed to Upload Photo: "+e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?>deleteCandidatePhoto(Candidate candidate){
+        Photo photo=photoRepository.findByCandidate(candidate);
+        if(photo!=null){
+            photoRepository.delete(photo);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    public ResponseEntity<?>deleteEmployerPhoto(Employer employer){
+        Photo photo=photoRepository.findByEmployer(employer);
+        if(photo!=null){
+            photoRepository.delete(photo);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    public ResponseEntity<?>updatePhoto(String url,MultipartFile file) throws IOException {
+        Photo photo=photoRepository.findByUrl(url);
+        if(photo!=null){
+            cloudinaryService.deleteImage(photo.getPublicId());
+            Map newPhoto=cloudinaryService.uploadImage(file);
+            photo.setUrl(newPhoto.get("url").toString());
+            photo.setPublicId(newPhoto.get("public_id").toString());
+            return  ResponseEntity.ok().body(photoRepository.save(photo));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
