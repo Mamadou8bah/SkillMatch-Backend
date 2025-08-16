a1f808802f1dbbfeb06ef9789a62c8f3b69b69c1
mamadou8bah <mbah18791@gmail.com>
2025-08-16 09:04:52 +0000

    Implemented photo repository

--- src/main/java/SkillMatch/repository/PhotoRepository.java
+++ src/main/java/SkillMatch/repository/PhotoRepository.java
@@ -1,4 +1,10 @@
 package SkillMatch.repository;
 
-public interface PhotoRepository {
+import SkillMatch.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    Photo findByUrl(String url);
+    Photo findByPublicId(String publicId);
 }
