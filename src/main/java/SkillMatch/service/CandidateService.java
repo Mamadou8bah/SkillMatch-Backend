83607a43fba3e1c26f03543ba60f78747bd80dda
mamadou8bah <mbah18791@gmail.com>
2025-08-16 09:47:15 +0000

    Implemented the logic for candidates to upload profile pic

--- src/main/java/SkillMatch/service/CandidateService.java
+++ src/main/java/SkillMatch/service/CandidateService.java
@@ -1,21 +1,32 @@
 package SkillMatch.service;
 import SkillMatch.exception.ResourceNotFoundException;
 import SkillMatch.model.Candidate;
+import SkillMatch.model.Photo;
 import SkillMatch.repository.CandidateRepo;
+import SkillMatch.repository.PhotoRepository;
 import jakarta.transaction.Transactional;
-
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CandidateService {
     @Autowired
     private CandidateRepo repo;
+
+    @Autowired
+    private PhotoService photoService;
+
+    @Autowired
+    private PhotoRepository photoRepository;
 
     @Transactional
     public List<Candidate>getCandidates(int page,int size){
@@ -47,6 +58,22 @@ public class CandidateService {
         update.setEducationHistory(candidate.getEducationHistory());
         update.setSkills(candidate.getSkills());
         return repo.save(update);
+    }
+
+    public ResponseEntity<?>uploadPhoto(MultipartFile file,Candidate candidate) throws IOException {
+        try {
+            Photo photo=photoService.createPhoto(file);
+            photo.setCandidate( candidate );
+            photoRepository.save(photo);
+            return ResponseEntity.ok().body("Image uploaded successfully");
+
+        }catch (IOException  e){
+            throw new IOException("Could not upload photo"+e.getMessage());
+        }
+    }
+    public ResponseEntity<?>deletePhoto(Photo photo)throws IOException{
+        photoService.deletePhoto(photo);
+        return ResponseEntity.ok().build();
     }
 
 }
