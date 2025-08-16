03c59fd7ca289e4b2d62209d906383265b6a485f
mamadou8bah <mbah18791@gmail.com>
2025-08-16 12:39:31 +0000

    Implemented the profile pic employers

--- src/main/java/SkillMatch/dto/RegisterRequest.java
+++ src/main/java/SkillMatch/dto/RegisterRequest.java
@@ -25,6 +25,5 @@ public class RegisterRequest {
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", 
          message = "Password must contain at least one uppercase letter, one lowercase letter, and one number")
     private String password;
-    private String profilePicture;
     private String location;
 }
--- src/main/java/SkillMatch/service/EmployerService.java
+++ src/main/java/SkillMatch/service/EmployerService.java
@@ -2,16 +2,29 @@ package SkillMatch.service;
 
 
 import SkillMatch.exception.ResourceNotFoundException;
+import SkillMatch.model.Candidate;
 import SkillMatch.model.Employer;
+import SkillMatch.model.Photo;
 import SkillMatch.repository.EmployerRepo;
+import SkillMatch.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
+import java.io.*;
 
 @Service
 public class EmployerService {
     @Autowired
     private EmployerRepo repo;
+
+    @Autowired
+    private PhotoService photoService;
+
+    @Autowired
+    private PhotoRepository photoRepository;
 
     public List<Employer> getEmployers(){
        List<Employer>employers=repo.findAll();
@@ -37,5 +50,26 @@ public class EmployerService {
         currentData.setIndustry(employer.getIndustry());
         return currentData;
 
+    }
+
+    public ResponseEntity<?>uploadPhoto(MultipartFile file, Employer employer) throws IOException {
+        try {
+            Photo photo=photoService.createPhoto(file);
+            photo.setEmployer(employer);
+            photoRepository.save(photo);
+            return ResponseEntity.ok().body("Image uploaded successfully");
+
+        }catch (IOException  e){
+            throw new IOException("Could not upload photo"+e.getMessage());
+        }
+    }
+    public ResponseEntity<?> deletePhoto(Photo photo)throws IOException{
+        photoService.deletePhoto(photo);
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<?>changeProfilePhoto(Photo photo, MultipartFile newPhoto)throws IOException{
+        photoService.updatePhoto(photo.getUrl(),newPhoto);
+        return ResponseEntity.ok().build();
     }
 }
